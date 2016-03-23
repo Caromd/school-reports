01752859e13fd2c4a3d7acd2a30179262b934bb9
caromd <rcdeppe@yahoo.co.uk>
2016-03-23 14:19:00 +0000

    Working on summary pdf

--- app/pdfs/annual_summary.rb
+++ app/pdfs/annual_summary.rb
@@ -2,9 +2,19 @@ class AnnualSummary < Prawn::Document
   def initialize
     super({:page_size => 'A4', :page_layout => :landscape})
     @terms = Term.where(year: '2016')
-    @grade = Grade.find_by(name: '12', year: '2016')
-    @grades = Grade.where(name: '12', year: '2016')
-    @markpercents = Markpercent.where(grade_id: @grade.id)
+    @level = Level.find_by(name: '12')
+    @grades = Grade.where(level_id: @level.id, year: '2016')
+    @markpercents = Markpercent.where(level_id: @level.id)
+    
+    @term1_classmark = "0";
+    @term1_testmark = "0";
+    @term2_classmark = "0";
+    @term2_testmark = "0";
+    @term3_classmark = "0";
+    @term3_testmark = "0";
+    @term4_classmark = "0";
+    @term4_testmark = "0";
+    
 #    @students = Student.where(id: @grade.student_id)
 #    @reports = Report.where(student_id: @students.id, term_id: @terms.id)
 #    @results = Result.where(report_id: @reports.id)
@@ -21,49 +31,66 @@ class AnnualSummary < Prawn::Document
 #    @number_of_subjects = 0
     @user = User.find(1)
 
-#    school_header
+school_header
 #    subject_header
-#    term_header
-#    table_content
-write_data
+    term_table_content
+#    item_rows
+    table_content
   end
 
-def write_data
+def school_header
   text @user.schoolname, size: 25, style: :bold, align: :center
-  text "GRADE " + @grade.name, size: 20, style: :bold, align: :center
+  text "GRADE " + @level.name, size: 20, style: :bold, align: :center
+end
+
+def item_rows
   @markpercents.map do |m|
     @subject = Subject.find_by_id(m.subject_id)
     text "SUBJECT " + @subject.name
 
     @grades.map do |g|
-      text "STUDENT ID " + g.student_id.to_s
       @student = Student.find_by_id(g.student_id)
-      text "STUDENT NAME " + @student.firstname
+      @student_name = @student.firstname + " " + @student.surname
       @terms.map do |t|
-        text "TERM...." + t.term
         @report = Report.find_by(student_id: g.student_id, term_id: t.id)
         @results = Result.where(report_id: @report.id, subject_id: m.subject_id)
         @results.map do |r|
-          text "RESULTS.........." + r.classmark.to_s + ' ' + @subject.name
+          case t.term
+          when "1"
+            @term1_classmark = r.classmark.to_s.strip;
+            @term1_testmark = r.testmark.to_s.strip;
+          when "2"
+            @term2_classmark = r.classmark.to_s.strip;
+            @term2_testmark = r.testmark.to_s.strip;
+          when "3"
+            @term3_classmark = r.classmark.to_s;
+            @term3_testmark = r.testmark.to_s;
+          when "4"
+            @term4_classmark = r.classmark.to_s;
+            @term4_testmark = r.testmark.to_s;
+          end
+#          text "RESULTS.........." + r.classmark.to_s + ' ' + @subject.name
         end
       end
     end
+#      text @student_name +  " "  + @term1_classmark +  " "  + @term1_testmark +  " "  + @term2_classmark +  " "  + @term2_testmark +  " "  + @term3_classmark +  " "  + @term3_testmark +  " "  + @term4_classmark +  " "  + @term4_testmark
+      [@student_name,@term1_classmark,@term1_testmark,@term2_classmark,@term2_testmark,@term3_classmark,@term3_testmark,@term4_classmark,@term4_testmark]
   end
 end
 
 
   ## SCHOOL NAME
   ## GRADE NAME
-  def school_header
-    y_position = cursor - 30
-    bounding_box([0, y_position], :width => 290, :height => 50) do
-      text @user.schoolname, size: 25, style: :bold, align: :center
-    end
-    y_position = cursor - 5
-    bounding_box([0, y_position], :width => 200, :height => 110) do
-      text "GRADE " + @grade.name, size: 20, style: :bold, align: :center
-    end
-  end
+#  def school_header
+#    y_position = cursor - 30
+#    bounding_box([0, y_position], :width => 290, :height => 50) do
+#      text @user.schoolname, size: 25, style: :bold, align: :center
+#    end
+#    y_position = cursor - 5
+#    bounding_box([0, y_position], :width => 200, :height => 110) do
+#      text "GRADE " + @grade.name, size: 20, style: :bold, align: :center
+#    end
+#  end
 
   def subject_header
     @subjects.map do |s|
@@ -76,26 +103,22 @@ end
   end
 
   def term_header
-    @terms_header = ""
-    @terms.map do |t|
-      @terms_header = @terms_header || t.name || ','
-    end
-    ["TERM1","TERM2","TERM3","TERM4"]
+    ["","TERM1","TERM2","TERM3","TERM4"]
   end
   
   def item_header
     ["PRAC","THEORY","PRAC","THEORY","PRAC","THEORY","PRAC","THEORY",]
   end
 
-  def item_rows
-    @results.map do |r|
-      @report = Report.find(r.report_id)
-      @student = Student.find(@report.student_id)
-      [@student.firstname, 
-      r.classmark.round, 
-      r.testmark.round]
-    end
-  end
+#  def item_rows
+#    @results.map do |r|
+#      @report = Report.find(r.report_id)
+#      @student = Student.find(@report.student_id)
+#      [@student.firstname, 
+#      r.classmark.round, 
+#      r.testmark.round]
+#    end
+#  end
   
   def item_average
 #    @average_total = @grand_total / @number_of_subjects
@@ -103,8 +126,23 @@ end
 [0,0,0,0,0,0,0]
   end
 
+  def term_table_data
+    [term_header] 
+  end
+
+  def term_table_content
+    table(term_table_data) do
+      row(0).font_style = :bold
+      self.row_colors = ["DDDDDD", "FFFFFF"]
+      self.header = true
+      self.cell_style = { size: 9 }
+      self.column_widths = [100,100,100,100,100]
+    end
+  end
+
   def item_table_data
-    [term_header, item_header, *item_rows, item_average] 
+#    [item_header, *item_rows] 
+[*item_rows]
   end
 
   def table_content
@@ -113,7 +151,7 @@ end
       self.row_colors = ["DDDDDD", "FFFFFF"]
       self.header = true
       self.cell_style = { size: 9 }
-      self.column_widths = [100,100,50,100,50,50,250]
+      self.column_widths = [100,50,50,50,50,50,50,50,50]
     end
   end
 end
