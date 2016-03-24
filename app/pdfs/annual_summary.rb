80ed727afda3575498c7c69bdcc2b4fac1b56436
caromd <rcdeppe@yahoo.co.uk>
2016-03-24 11:02:54 +0000

    Working on summary pdf

--- app/pdfs/annual_summary.rb
+++ app/pdfs/annual_summary.rb
@@ -14,6 +14,9 @@ class AnnualSummary < Prawn::Document
     @term3_testmark = "0";
     @term4_classmark = "0";
     @term4_testmark = "0";
+    @mark1_label = "C";
+    @mark2_label = "T";
+    @subject_id = 0;
     
 #    @students = Student.where(id: @grade.student_id)
 #    @reports = Report.where(student_id: @students.id, term_id: @terms.id)
@@ -31,95 +34,65 @@ class AnnualSummary < Prawn::Document
 #    @number_of_subjects = 0
     @user = User.find(1)
 
-school_header
-#    subject_header
-    term_table_content
+# school_header
+    subject_header
+#    term_table_content
 #    item_rows
-    table_content
+#    table_content
   end
 
-def school_header
-  text @user.schoolname, size: 25, style: :bold, align: :center
-  text "GRADE " + @level.name, size: 20, style: :bold, align: :center
+def subject_header
+  @markpercents.map do |m|
+    text @user.schoolname, size: 25, style: :bold, align: :center
+    text "GRADE " + @level.name, size: 20, style: :bold, align: :center
+    @subject = Subject.find_by_id(m.subject_id)
+    @subject_id = m.subject_id
+    text @subject.name, size: 18, style: :bold, align: :center
+    @mark1_label = m.mark1_label + " (" + m.mark1_percentage.to_s + "%)"
+    @mark2_label = m.mark2_label + " (" + m.mark2_percentage.to_s + "%)"
+    term_table_content
+    table_content
+    start_new_page
+  end
 end
 
 def item_rows
-  @markpercents.map do |m|
-    @subject = Subject.find_by_id(m.subject_id)
-    text "SUBJECT " + @subject.name
-
     @grades.map do |g|
       @student = Student.find_by_id(g.student_id)
       @student_name = @student.firstname + " " + @student.surname
       @terms.map do |t|
         @report = Report.find_by(student_id: g.student_id, term_id: t.id)
-        @results = Result.where(report_id: @report.id, subject_id: m.subject_id)
+        @results = Result.where(report_id: @report.id, subject_id: @subject.id)
+        @classmark = 0;
+        @testmark = 0;
         @results.map do |r|
-          case t.term
+          @classmark = r.classmark.to_s.strip;
+          @testmark = r.testmark.to_s.strip;
+        end
+        case t.term
           when "1"
-            @term1_classmark = r.classmark.to_s.strip;
-            @term1_testmark = r.testmark.to_s.strip;
+            @term1_classmark = @classmark;  @term1_testmark = @testmark;
           when "2"
-            @term2_classmark = r.classmark.to_s.strip;
-            @term2_testmark = r.testmark.to_s.strip;
+            @term2_classmark = @classmark;  @term2_testmark = @testmark;
           when "3"
-            @term3_classmark = r.classmark.to_s;
-            @term3_testmark = r.testmark.to_s;
+            @term3_classmark = @classmark;  @term3_testmark = @testmark;
           when "4"
-            @term4_classmark = r.classmark.to_s;
-            @term4_testmark = r.testmark.to_s;
-          end
-#          text "RESULTS.........." + r.classmark.to_s + ' ' + @subject.name
+            @term4_classmark = @classmark;  @term4_testmark = @testmark;
         end
       end
-    end
 #      text @student_name +  " "  + @term1_classmark +  " "  + @term1_testmark +  " "  + @term2_classmark +  " "  + @term2_testmark +  " "  + @term3_classmark +  " "  + @term3_testmark +  " "  + @term4_classmark +  " "  + @term4_testmark
       [@student_name,@term1_classmark,@term1_testmark,@term2_classmark,@term2_testmark,@term3_classmark,@term3_testmark,@term4_classmark,@term4_testmark]
   end
 end
-
-
-  ## SCHOOL NAME
-  ## GRADE NAME
-#  def school_header
-#    y_position = cursor - 30
-#    bounding_box([0, y_position], :width => 290, :height => 50) do
-#      text @user.schoolname, size: 25, style: :bold, align: :center
-#    end
-#    y_position = cursor - 5
-#    bounding_box([0, y_position], :width => 200, :height => 110) do
-#      text "GRADE " + @grade.name, size: 20, style: :bold, align: :center
-#    end
-#  end
-
-  def subject_header
-    @subjects.map do |s|
-      y_position = cursor - 5
-      bounding_box([0, y_position], :width => 200, :height => 110) do
-        text s.name, size: 20, style: :bold, align: :center
-      end
-      term_header
-    end
-  end
 
   def term_header
     ["","TERM1","TERM2","TERM3","TERM4"]
   end
   
   def item_header
-    ["PRAC","THEORY","PRAC","THEORY","PRAC","THEORY","PRAC","THEORY",]
+    ["",@mark1_label,@mark2_label,@mark1_label,@mark2_label,@mark1_label,@mark2_label,@mark1_label,@mark2_label]
   end
 
-#  def item_rows
-#    @results.map do |r|
-#      @report = Report.find(r.report_id)
-#      @student = Student.find(@report.student_id)
-#      [@student.firstname, 
-#      r.classmark.round, 
-#      r.testmark.round]
-#    end
-#  end
-  
   def item_average
 #    @average_total = @grand_total / @number_of_subjects
 #    ["Average", "",@average_class, "", @average_test, @average_total.round, ""]
@@ -141,8 +114,8 @@ end
   end
 
   def item_table_data
-#    [item_header, *item_rows] 
-[*item_rows]
+    [item_header, *item_rows] 
+#[*item_rows]
   end
 
   def table_content
